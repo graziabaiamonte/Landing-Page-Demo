315160384b9857aec7e80f17cb57fc21d357a411
graziabaiamonte <graziabaiamonte19@gmail.com>
2024-04-22 21:26:44 +0200

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   
+   */
   output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+
+   */
+  basePath: "/Landing-page-demo",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+ 
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
